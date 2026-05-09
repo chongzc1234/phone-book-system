4b3188120783ebe7be87015dd40fea9611bbda6c
chong <chongzc1234@gmail.com>
2026-05-09 19:17:28 +0800

    create migration for contacts table with fields for user_id, name, phone, email, image_path, created_at, and updated_at

---
+++ app/Database/Migrations/2026-05-09-110317_CreateContactsTable.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateContactsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+                    'id' => [
+                        'type'           => 'INT',
+                        'constraint'     => 11,
+                        'unsigned'       => true,
+                        'auto_increment' => true,
+                    ],
+                    'user_id' => [
+                        'type'       => 'INT',
+                        'constraint' => 11,
+                        'unsigned'   => true,
+                    ],
+                    'name' => [
+                        'type'       => 'VARCHAR',
+                        'constraint' => '100',
+                    ],
+                    'phone' => [
+                        'type'       => 'VARCHAR',
+                        'constraint' => '20',
+                    ],
+                    'email' => [
+                        'type'       => 'VARCHAR',
+                        'constraint' => '100',
+                        'null'       => true,
+                    ],
+                    'image_path' => [
+                        'type'       => 'VARCHAR',
+                        'constraint' => '255',
+                        'default'    => 'default.png',
+                    ],
+                    'created_at' => [
+                        'type' => 'DATETIME',
+                        'null' => true,
+                    ],
+                    'updated_at' => [
+                        'type' => 'DATETIME',
+                        'null' => true,
+                    ],
+                ]);
+
+                $this->forge->addKey('id', true);
+                $this->forge->addForeignKey('user_id', 'users', 'id', 'CASCADE', 'CASCADE');
+                $this->forge->createTable('contacts');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('contacts');
+    }
+}
